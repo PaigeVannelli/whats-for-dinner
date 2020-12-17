860fa03580ee7078a0605a78870264e5cb3af7f4
Paige Vannelli <paigevannelli@gmail.com>
2020-12-16 17:21:25 -0700

    Finish functionality for displaying random food item

--- main.js
+++ main.js
@@ -2,35 +2,40 @@
 
 window.addEventListener("click", checkForm);
 
-//~~~~~~~~~~~~~~~~~~~~ GLOBAL VARIABLES ~~~~~~~~~~~~~~~~~~~~~//
+//~~~~~~~~~~~~~~~~~~~~ To Dd ~~~~~~~~~~~~~~~~~~~~~//
 
 //If I click on one - make sure all other thre are false
+
+//for checkForm function
+//determine if something is checked and return name
+//maybe iterate through and retunr
+//query selector all - if check give me the id
+// arry of side main dessert iterate through and pass them in and see if they are checked
+
+//Add hidden and remove hidden functions
+//
 
 //~~~~~~~~~~~~~~~~~~~ FUNCTIONS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
 
 
 function checkForm() {
-  //determine if something is checked and return name
-  //maybe iterate through and retunr
-  //query selector all - if check give me the id
-  // arry of side main dessert iterate through and pass them in and see if they are checked
-  var side = document.getElementById("side").checked
-  var main = document.getElementById("main").checked
-  var dessert = document.getElementById("dessert").checked
-  var entireMeal = document.getElementById("entire-meal").checked
-  clickReturn(side, main, dessert, entireMeal)
+  var side = document.getElementById("side").checked;
+  var main = document.getElementById("main").checked;
+  var dessert = document.getElementById("dessert").checked;
+  var entireMeal = document.getElementById("entire-meal").checked;
+  clickReturn(side, main, dessert, entireMeal);
 }
 
 function clickReturn(side, main, dessert, entireMeal) {
-  var buttonClick = event.target.className
+  var buttonClick = event.target.className;
   if (buttonClick === "lets-cook-button") {
     event.preventDefault(buttonClick);
-    returnSpecificMeal(side, main, buttonClick);
-    displayFood()
+    returnFoodItemHandler(side, main, dessert, entireMeal, buttonClick);
+    displayFood();
   }
 }
 
-function returnSpecificMeal(side, main, buttonClick) {
+function returnFoodItemHandler(side, main, dessert, entireMeal, buttonClick) {
   if (side && buttonClick === "lets-cook-button") {
     returnFoodItem(sides);
   } else if (main && buttonClick === "lets-cook-button") {
@@ -38,12 +43,11 @@ function returnSpecificMeal(side, main, buttonClick) {
   } else if (dessert && buttonClick === "lets-cook-button") {
     returnFoodItem(desserts);
   } else if (entireMeal && buttonClick === "lets-cook-button") {
-    // console.log("return meal")
   }
 }
 
 function returnFoodItem(array) {
-  console.log(array[getRandomIndex(array)])
+  document.querySelector(".random-recipe").innerText = array[getRandomIndex(array)];
 }
 
 function getRandomIndex(array) {
@@ -56,20 +60,13 @@ function displayFood() {
 }
 
 function hidePot() {
-  var cookpot = document.querySelector(".cookpot")
+  var cookpot = document.querySelector(".cookpot");
   cookpot.classList.add("hidden");
 }
 
 function showRecipe() {
   var recipeDisplay = document.querySelector(".recipe-display");
   var randomRecipeDisplay = document.querySelector(".random-recipe");
-  recipeDisplay.classList.remove("hidden")
-  randomRecipeDisplay.classList.remove("hidden")
+  recipeDisplay.classList.remove("hidden");
+  randomRecipeDisplay.classList.remove("hidden");
 }
-// function addHidden(cookpot) {
-//   // var cookpot = document.querySelector(`.${element}`)
-//   cookpot.classList.add(".hidden");
-// }
-// function removeHidden() {
-//
-// }
